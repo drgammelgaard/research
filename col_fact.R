ba1943a52b17f51a65bd03ab988e773aed20f830
agdamsbo <43278243+agdamsbo@users.noreply.github.com>
2018-09-27 11:52:59 +0200

    Update col_fact.R

--- col_fact.R
+++ col_fact.R
@@ -1,9 +1,14 @@
 col_fact<-function(string,data){
-## Defining factors for columns containing string, based on dplyr
+## Defining factors for columns containing string (can be vector of multiple strings), based on dplyr
 
   require(dplyr)
   d<-data
-  n<-names(select(d,contains(string)))
+  s<-string
+  n<-c()
+  
+  for(i in 1:length(s)){
+  n<-c(n,names(select(d,contains(s[i])))
+  }
   
   for(i in 1:length(n)) {
     d[,n[i]]<-factor(d[,n[i]])
