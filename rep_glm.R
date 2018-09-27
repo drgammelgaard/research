36da04c96562bc2885fe2ab0dff61b3105da770e
agdamsbo <43278243+agdamsbo@users.noreply.github.com>
2018-09-27 11:29:07 +0200

    Update rep_glm.R

--- rep_glm.R
+++ rep_glm.R
@@ -7,13 +7,19 @@ rep_glm<-function(x,y){
   for(i in 1:ncol(x)){
      m<-glm(y~x[,i],family = binomial())
      
+     v<-x[,i]
+     
      b<-round(coef(m)[-1],3)
      
      pv<-round(tidy(m)$p.value[-1],3)
-    pv<-ifelse(pv<0.001,"<0.001",pv)
-    pv <- ifelse(pv<=0.05|pv=="<0.001",paste0("*",pv),pv)
+     pv<-ifelse(pv<0.001,"<0.001",pv)
+     pv <- ifelse(pv<=0.05|pv=="<0.001",paste0("*",pv),pv)
      
-     pred<-names(x)[i]
+     if (is.factor(v)){
+       pred<-paste(names(x)[i],levels(v)[-1],sep = "_")}
+       
+       else {pred<-names(x)[i]}
+     
      
      df<-rbind(df,cbind(pred,b,pv))
      }
