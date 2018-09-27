67fbc6b762799b37f6aa87a270fbb43a95cce78a
agdamsbo <43278243+agdamsbo@users.noreply.github.com>
2018-09-27 11:38:51 +0200

    Create col_fact.R

---
+++ col_fact.R
@@ -0,0 +1,12 @@
+col_fact<-function(string,data){
+## Defining factors for columns containing string, based on dplyr
+
+  require(dplyr)
+  d<-data
+  n<-names(select(d,starts_with(string)))
+  
+  for(i in 1:length(n)) {
+    d[,n[i]]<-factor(d[,n[i]])
+  }
+  return(d)
+}
