30740ff4893a44d5a17420f11cc760f27e55fb16
agdamsbo <43278243+agdamsbo@users.noreply.github.com>
2018-09-27 12:14:23 +0200

    Update col_fact.R

--- col_fact.R
+++ col_fact.R
@@ -1,6 +1,6 @@
 col_fact<-function(string,data,levels=NULL,labels=NULL){
 ## Defining factors for columns containing string (can be vector of multiple strings), based on dplyr.
-## Factoring several columns with same levels, these can be provided.
+## Factoring several columns with same levels or labels, these can be provided.
   
   require(dplyr)
   d<-data
@@ -11,8 +11,17 @@ col_fact<-function(string,data,levels=NULL,labels=NULL){
   n<-c(n,names(select(d,contains(s[i]))))
   }
 
+  if (!is.null(levels){
     for(i in 1:length(n)) {
-    d[,n[i]]<-factor(d[,n[i]],levels=levels,labels=labels)
-  }
+    d[,n[i]]<-factor(d[,n[i]],levels=levels)}}
+    
+    if (!is.null(labels){ 
+      for(i in 1:length(n)) {
+        d[,n[i]]<-factor(d[,n[i]],labels=labels)
+  }}
+      else 
+        for(i in 1:length(n)) {
+          d[,n[i]]<-factor(d[,n[i]])}
+  
   return(d)
 }
