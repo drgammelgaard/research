7a597d1acf9a2b450ce90d0073fa35d901b5cc8b
agdamsbo <43278243+agdamsbo@users.noreply.github.com>
2018-09-28 08:41:41 +0200

    Update rep_glm.R

--- rep_glm.R
+++ rep_glm.R
@@ -1,37 +1,12 @@
-rep_glm<-function(x,y,output="coef"){
-## x is data.frame, y is vector
-## output can be "coef" or "or_ci"
-## The confint() function is rather slow, causing the whole function to hang when including many predictors.
+rep_glm<-function(x,y,or=FALSE){
+## x is data.frame of predictors, y is vector of an aoutcome as a factor
+## output is returned as coefficient, or if or=TRUE as OR with 95 % CI.
+## The confint() function is rather slow, causing the whole function to hang when including many predictors and calculating the ORs with CI.
   
   require(broom)
-
+if (!is.factor(y)){stop("Some kind of error message would be nice, but y should be a factor!")}
   
-   if (output=="coef"){
-     
-       df<-data.frame(matrix(ncol = 3))
-  names(df)<-c("pred","b","pv")
-     
-  for(i in 1:ncol(x)){
-     m<-glm(y~x[,i],family = binomial())
-     
-     b<-round(coef(m)[-1],3)
-     
-     pv<-round(tidy(m)$p.value[-1],3)
-     pv<-ifelse(pv<0.001,"<0.001",pv)
-     pv <- ifelse(pv<=0.05|pv=="<0.001",paste0("*",pv),pv)
-    pv <- ifelse(pv>0.05&pv<=0.1,paste0(".",pv),pv)
-     
-     v<-x[,i]
-     
-     if (is.factor(v)){
-       pred<-paste(names(x)[i],levels(v)[-1],sep = "_")}
-       
-       else {pred<-names(x)[i]}
-     
-     df<-rbind(df,cbind(pred,b,pv))}
-     }
-   
-    if (output=="or_ci"){
+    if (or==TRUE){
       
              df<-data.frame(matrix(ncol = 3))
   names(df)<-c("pred","or_ci","pv")
@@ -43,24 +18,51 @@ rep_glm<-function(x,y,output="coef"){
      u<-suppressMessages(round(exp(confint(m))[-1,2],2))
      or<-round(exp(coef(m))[-1],2)
      
-     (or_ci<-paste0(or," (",l," to ",u,")"))
+     or_ci<-paste0(or," (",l," to ",u,")")
      
      pv<-round(tidy(m)$p.value[-1],3)
      pv<-ifelse(pv<0.001,"<0.001",pv)
-     pv <- ifelse(pv<=0.05|pv=="<0.001",paste0("*",pv),pv)
-    pv <- ifelse(pv>0.05&pv<=0.1,paste0(".",pv),pv)
+     pv <- ifelse(pv<=0.05|pv=="<0.001",paste0("*",pv),
+                  ifelse(pv>0.05&pv<=0.1,paste0(".",pv),pv))
+
      
     v<-x[,i]
      
     if (is.factor(v)){
-       pred<-paste(names(x)[i],levels(v)[-1],sep = "_")}
+       pred<-paste(names(x)[i],levels(v)[-1],sep = "_")
+    }
        
        else {pred<-names(x)[i]}
       
       df<-rbind(df,cbind(pred,or_ci,pv))
+      
     }}
     
-      else {df="Some kind of error message would be nice"}
+  if (or==FALSE){
      
+       df<-data.frame(matrix(ncol = 3))
+  names(df)<-c("pred","b","pv")
+     
+  for(i in 1:ncol(x)){
+     m<-glm(y~x[,i],family = binomial())
+     
+     b<-round(coef(m)[-1],3)
+     
+     pv<-round(tidy(m)$p.value[-1],3)
+     pv<-ifelse(pv<0.001,"<0.001",pv)
+     pv <- ifelse(pv<=0.05|pv=="<0.001",paste0("*",pv),
+                  ifelse(pv>0.05&pv<=0.1,paste0(".",pv),pv))
+     
+     v<-x[,i]
+     
+     if (is.factor(v)){
+       pred<-paste(names(x)[i],levels(v)[-1],sep = "_")
+     }
+       
+       else {pred<-names(x)[i]}
+     
+     df<-rbind(df,cbind(pred,b,pv))
+  }}
+  
   return(df)
 }
